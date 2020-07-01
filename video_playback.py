349af06cc6f2019e12b83864101de9bdda348ec4
mohammadathania <mohammad.athania@somaiya.edu>
2020-07-01 20:22:14 +0530

    Video Playback

---
+++ video_playback.py
@@ -0,0 +1,31 @@
+import cv2
+frames = []
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    x , frame = cap.read()
+    frames.append(frame)
+    cv2.imshow('Video Capture', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+print(len(frames))
+
+def nothing(arg): pass
+cv2.namedWindow('Video Playback')
+cv2.createTrackbar('Frame Number', 'Video Playback', 0,len(frames), nothing)
+
+while True:
+    frame_number = cv2.getTrackbarPos('Frame Number', 'Video Playback')
+    print(frame_number)
+    try:
+        img = frames[frame_number]
+    except :
+        print('Frames Completed')
+    cv2.imshow('Video Playback', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
